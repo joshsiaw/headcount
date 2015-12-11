5722a81017fb817398820d85dc337820f7256906
joshsiaw <joshsiaw@gmail.com>
2015-12-11 19:04:54 +0800

    Added drop down options for Event and Attendee

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,5 +1,6 @@
 class Attendee < ActiveRecord::Base
   has_many :attendances
+  has_many :events, through: :attendances
 
   validates :first_name, presence: true
   validates :contact_no, presence: true
@@ -7,4 +8,29 @@ class Attendee < ActiveRecord::Base
   validates :category, presence: true
   validates :group, presence: true
 
+  scope :not_present, -> (event) { where.not(id: event.attendees.pluck(:id)) }
+
+  CATEGORY =
+    [
+      ["Newcomer", "newcomer"],
+      ["Regular", "regular"],
+      ["Core", "core"],
+      ["Leader", "leader"]
+    ]
+
+  GROUP =
+    [
+      ["Forge", "forge"],
+      ["Central", "central"],
+      ["Ezer", "ezer"],
+      ["Neuvo", "neuvo"]
+    ]
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def initials
+    "#{first_name.try(:[], 0)}#{last_name.try(:[], 0)}"
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,14 @@
 class Event < ActiveRecord::Base
-  has_many :attendances
+  has_many :attendances, dependant: :destroy
+  has_many :attendees, through: :attendances
 
   validates :date, presence: true
   validates :category, presence: true
+
+  CATEGORY =
+    [
+      ["Service", "service"],
+      ["Outreach", "outreach"],
+      ["Other", "other"]
+    ]
 end
