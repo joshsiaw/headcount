3fd2973e21615f067f7bc668523365e2cb162b60
joshsiaw <joshsiaw@gmail.com>
2016-04-05 16:32:53 +0800

    Clean up report

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,7 @@ class Report < ActiveRecord::Base
   belongs_to :attendee_group
 
   validates :event_id, presence: true
+  validates :attendee_group_id, presence: true
   validates :author, presence: true
   validates :group, presence: true
 end
